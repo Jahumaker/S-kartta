31495919c154204cfcb27892c3b0bfda62a93d49
Jahumaker <janne.huuskonen@stt.fi>
2026-07-21 17:49:51 +0300

    Update fetch_weather.py

--- scripts/fetch_weather.py
+++ scripts/fetch_weather.py
@@ -26,6 +26,22 @@ CITIES = {
     "Mikkeli": (61.69, 27.27),
     "Kotka": (60.47, 26.94),
     "Kokkola": (63.84, 23.13),
+    # Added cities for denser forecast map
+    u"H\u00e4meenlinna": (60.99, 24.46),
+    "Savonlinna": (61.87, 28.88),
+    "Kemi": (65.73, 24.56),
+    "Kouvola": (60.87, 26.70),
+    u"Maarianhamina": (60.10, 19.93),
+    "Rauma": (61.13, 21.51),
+    u"Salo": (60.38, 23.13),
+    "Iisalmi": (63.56, 27.19),
+    u"Ylivieska": (63.84, 24.54),
+    u"Muonio": (67.93, 23.68),
+    u"Enonteki\u00f6": (68.41, 23.63),
+    "Inari": (69.07, 27.02),
+    u"Nurmes": (63.54, 29.14),
+    "Lieksa": (63.32, 30.02),
+    u"Kuusamo": (65.97, 29.19),
 }
 
 
